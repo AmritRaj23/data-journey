25d3ad7faa71fce4aab26d2905b237019c418054
Amrit Raj <14956443+AmritRaj23@users.noreply.github.com>
2022-08-30 06:13:19 +0200

    Add files via upload

--- day-1/ecommerce-streaming/cloud-run-pubsub-proxy/app.js
+++ day-1/ecommerce-streaming/cloud-run-pubsub-proxy/app.js
@@ -60,71 +60,7 @@ app.post('/json', (req, res) => {
       res.status(500).send(err);
       return Promise.reject(err);
    });
-
-  // console.log(`received /post ${data}`)
-  // res.send(`received /post ${data}`);
 })
 
-app.get('/pixel', (req, res) => {
-  console.log('Send data to Pub/Sub');
-
-  // [START functions_pubsub_setup]
-  const {PubSub} = require('@google-cloud/pubsub');
-
-  // Instantiates a client
-  const pubsub = new PubSub();
-
-  const {Buffer} = require('safe-buffer');
-
-  // Set Pub/Sub topic name
-  let topicName = 'retail-stream';
-
-  // References an existing topic
-  const topic = pubsub.topic(topicName);
-
-  const message = {
-    data: {
-      // eventTimestamp: 'req.body.eventTimestamp',
-      message: {
-        foo: "bar",
-        hello: "world",
-        url: req.url,
-        params: req.params,
-        query: req.query
-      }
-    },
-  };
-
-  // // build pub/sub message
-  // const pubSubMessage = req.body.message;
-
-  // const target = process.env.TARGET || 'World';
-
-  // const message = req.body
-
-  // convert object to string
-  const data = JSON.stringify(message)
-
-  // Publishes the message as a string, 
-  const dataBuffer = Buffer.from(data);
-
-  // Add two custom attributes, origin and username, to the message
-  const customAttributes = {
-    origin: 'gtm-cloud-run',
-    username: 'gcp-demo',
-  };
-
-  // Publishes a message to Pub/Sub
-  return topic
-    .publish(dataBuffer, customAttributes)
-    .then(() => res.status(200).send(`{"message": "pubsub message sent: ${dataBuffer}"}`))
-    .catch(err => {
-      console.error(err);
-      res.status(500).send(err);
-      return Promise.reject(err);
-   });
-
-  // res.send(`Data sent ${dataBuffer}!`);
-});
 
 module.exports = app;
